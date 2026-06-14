50864a9c308ea50b6a79d8dc23e6f65f573fc07f
Lakum Hardip <lakumhardip11@gmail.com>
2026-06-14 11:40:22 +0530

    Code and Photos Edit

--- js/javascript.js
+++ js/javascript.js
@@ -99,3 +99,38 @@ window.addEventListener("scroll", function () {
         header.classList.remove("scrolled");
     }
 });
+
+
+const swiper = new Swiper(".mySwiper", {
+
+    loop: true,
+
+    spaceBetween: 30,
+
+    grabCursor: true,
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+
+        0: {
+            slidesPerView: 1
+        },
+
+        768: {
+            slidesPerView: 2
+        },
+
+        992: {
+            slidesPerView: 3
+        }
+    }
+});
